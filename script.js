04f7a18e496647e6be20db8a0b3848b7f1747407
Ricoding <rikardoan@gmail.com>
2023-03-20 20:13:45 -0400

    Update script.js
    new script

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-const fs = require('fs');
-const path = require('path');
-
 const folderPath = './scripts';
 const scriptLinks = document.getElementById('script-links');
 
-fs.readdir(folderPath, (err, files) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+fetch(folderPath)
+  .then((response) => response.text())
+  .then((html) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const files = [...doc.querySelectorAll('a')].map((a) => a.href);
 
-  const links = files.map((file) => {
-    const filePath = path.join(folderPath, file);
-    const fileName = path.basename(file);
-    return `<li><a href="${filePath}">${fileName}</a></li>`;
-  });
+    const links = files.map((file) => {
+      const fileName = file.split('/').pop();
+      return `<li><a href="${file}">${fileName}</a></li>`;
+    });
 
-  const linksString = links.join('\n');
-  scriptLinks.innerHTML = linksString;
-});
+    const linksString = links.join('\n');
+    scriptLinks.innerHTML = linksString;
+  })
+  .catch((err) => console.error(err));
